c1d4e8789e6a19ab8080e734cd3657e1382d2b44
ctuu <ct.liuu@gmail.com>
2017-06-15 09:59:05 +0800

    commit

--- SGMS/main.c
+++ SGMS/main.c
@@ -7,20 +7,21 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 bool Stu_Add(List *plist);
-void Stu_Delete(List *plist);
+bool Stu_Delete(List *plist);
 void Stu_Search(const List *plist);
-void Stu_Modify(List *plist);
-void Stu_Sort(List *plist);
-void Stu_insert(List *plist);
-void Stu_Display(const List *plist);
+bool Stu_Modify(List *plist);
+bool Stu_Sort(List *plist);
+bool Stu_insert(List *plist);
+bool Stu_Display(const List *plist);
 void Stu_Statistic(const List *plist);
 void Stu_Saved(const List *plist);
-
+void Stu_Exit(List *plist);
 bool Input_Item(Item *item, List *plist);
 void Item_Display(const Item item);
 void Title_Display(void);
 
 char Menu(void);
+char Search_Menu(void);
 char Modify_Menu(void);
 char Sort_Menu(void);
 
@@ -28,13 +29,13 @@ void Stat_GetMin(Grade *gcur, const Grade *head, bool inorder);
 void Stat_Title(void);
 void Stat_Display(Grade *g_min, Grade *g_max, Grade *g_tot, Grade *g_failed, const List *plist);
 
+bool unsaved = 0;
 int main(void)
 {
     List stu;
     InitializeList(&stu);
 
     FILE *fp;
-    
 
     if ((fp = fopen("sgms.txt", "r")) == NULL)
     {
@@ -49,11 +50,13 @@ int main(void)
     char chooce;
     while ((chooce = Menu()) != '0')
     {
+        printf("\n");
         switch (chooce)
         {
         case '1':
             while (Stu_Add(&stu))
             {
+                unsaved = 1;
                 printf("enter 'Y' to add next record(other key to cancel): ");
                 char temp[2];
                 if (s_gets(temp, 2) == NULL || temp[0] == '\0' || strchr("yY", temp[0]) == NULL)
@@ -61,19 +64,23 @@ int main(void)
             }
             break;
         case '2':
-            Stu_Delete(&stu);
+            if (Stu_Delete(&stu))
+                unsaved = 1;
             break;
         case '3':
             Stu_Search(&stu);
             break;
         case '4':
-            Stu_Modify(&stu);
+            if (Stu_Modify(&stu))
+                unsaved = 1;
             break;
         case '5':
-            Stu_Sort(&stu);
+            if (Stu_Sort(&stu))
+                unsaved = 1;
             break;
         case '6':
-            Stu_insert(&stu);
+            if (Stu_insert(&stu))
+                unsaved = 1;
             break;
         case '7':
             Stu_Display(&stu);
@@ -91,37 +98,37 @@ int main(void)
         while (getchar() != '\n')
             continue;
     }
+    Stu_Exit(&stu);
     EmptyTheList(&stu);
-
     puts("Bye.");
     return 0;
 }
 
 char Menu(void)
 {
-    char ch;
-
     printf("\n----The Students' Grade Management System----\n");
-    puts("Enter the number to your choice:");
     puts("1) add     record             2) delete record");
     puts("3) search  record             4) modify record");
     puts("5) sort    record             6) insert record");
     puts("7) display record             8) statistics");
     puts("9) save record                0) quit");
-    while ((ch = getchar()) != EOF)
+    puts("Enter the number to your choice(1 ~ 9 or 0):");
+
+    char ch[2];
+    while (s_gets(ch, 2) != NULL && ch[0] != '\0')
     {
-        while (getchar() != '\n')
-            continue;
-        ch = tolower(ch);
-        if (strchr("1234567890", ch) == NULL)
+        ch[0] = tolower(ch[0]);
+        if (strchr("1234567890", ch[0]) == NULL)
             puts("ERROR: Please enter 1 ~ 9 or 0:");
         else
             break;
     }
-    if (ch == EOF)
-        ch = '0';
-
-    return ch;
+    if (ch[0] == '\0')
+    {
+        ch[0] = '0';
+        puts("Operation canceled.");
+    }
+    return ch[0];
 }
 
 bool Stu_Add(List *plist)
@@ -179,22 +186,23 @@ bool Input_Item(Item *item, List *plist)
     return true;
 }
 
-void Stu_Delete(List *plist)
+bool Stu_Delete(List *plist)
 {
     Item temp;
     if (ListIsEmpty(plist))
     {
         puts("ERROR: No entries!");
-        return;
+        return false;
     }
     Stu_Display(plist);
     puts("Please enter the ID of Student you wish to delete:");
     if (!Get_ID(temp.StuID))
-        return;
+        return false;
     if (ListDeleteItem(&temp, plist))
         printf("ID %s is dropped from the System.\n", temp.StuID);
     else
         printf("ERROR: Id %s is not a member.\n", temp.StuID);
+    return true;
 }
 
 void Stu_Search(const List *plist)
@@ -205,71 +213,88 @@ void Stu_Search(const List *plist)
         puts("ERROR: No entries!");
         return;
     }
-
-    puts("Please chooce the way of search: ");
-    puts("1) ID           2) Name");
-    char ch;
-    while ((ch = getchar()) != EOF)
+    char chooce;
+    Node *found = NULL;
+    while ((chooce = Search_Menu()) != '0')
     {
-        while (getchar() != '\n')
-            continue;
-        ch = tolower(ch);
-        if (strchr("12", ch) == NULL)
-            puts("ERROR: Please enter 1 or 2:");
-        else
+        switch (chooce)
+        {
+        case '1':
+            if (!Get_ID(temp.StuID))
+                return;
+            found = ListSeekSet(&temp, plist, seek_bID);
+            if (found == NULL)
+                printf("ERROR: ID %s is not a member.\n", temp.StuID);
+            else
+            {
+                Title_Display();
+                Item_Display(found->item);
+            }
             break;
-    }
-    if (ch == '1')
-    {
-        Node *found = NULL;
-        if (!Get_ID(temp.StuID))
-            return;
-        found = ListSeekSet(&temp, plist, seek_bID);
-        if (found == NULL)
-            printf("ERROR: ID %s is not a member.\n", temp.StuID);
-        else
-        {
-            Title_Display();
-            Item_Display(found->item);
-        }
-    }
-    else
-    {
-        puts("Please enter Student's Name:");
-        if (s_gets(temp.Name, LEN_NAME) == NULL || temp.Name[0] == '\0')
-        {
-            puts("Operation canceled");
-            return false;
-        }
-        uppercase(temp.Name);
-        if (!InList(&temp, plist, seek_bName))
-            printf("ERROR: Name %s is not a member.\n", temp.Name);
-        else
-        {
-            Title_Display();
-            ListSeekMultiSet(&temp, plist, seek_bName, Item_Display);
+        case '2':
+            puts("Please enter Student's Name:");
+            if (s_gets(temp.Name, LEN_NAME) == NULL || temp.Name[0] == '\0')
+            {
+                puts("Operation canceled");
+                return false;
+            }
+            uppercase(temp.Name);
+            if (!InList(&temp, plist, seek_bName))
+                printf("ERROR: Name %s is not a member.\n", temp.Name);
+            else
+            {
+                Title_Display();
+                ListSeekMultiSet(&temp, plist, seek_bName, Item_Display);
+            }
+            break;
+        default:
+            puts("ERROR: Switching error");
         }
     }
 }
 
-void Stu_Modify(List *plist)
+char Search_Menu(void)
 {
+    puts("--------------Search--------------");
+    puts("1) ID                2) Name");
+    puts("0) cancel");
+    puts("Please chooce the way of search:");
+    char ch[2];
+    while (s_gets(ch, 2) != NULL && ch[0] != '\0')
+    {
+        ch[0] = tolower(ch[0]);
+        if (strchr("012", ch[0]) == NULL)
+            puts("ERROR: Please enter 1, 2 or 0:");
+        else
+            break;
+    }
+    if (ch[0] == '\0')
+    {
+        ch[0] = '0';
+        puts("Operation canceled.");
+    }
+    return ch[0];
+}
+
+bool Stu_Modify(List *plist)
+{
+    bool isMod = 0;
     if (ListIsEmpty(plist))
     {
         puts("ERROR: No entries!");
-        return;
+        return false;
     }
     Stu_Display(plist);
 
     Item temp;
     Node *fnode;
     if (!Get_ID(temp.StuID))
-        return;
+        return false;
     fnode = ListSeekSet(&temp, plist, seek_bID);
     if (fnode == NULL)
     {
         printf("ERROR: ID %s is not a member.\n", temp.StuID);
-        return;
+        return false;
     }
     Title_Display();
     Item_Display(fnode->item);
@@ -289,6 +314,7 @@ void Stu_Modify(List *plist)
                 printf("ERROR: ID %s is exist.\n", temp.StuID);
                 continue;
             }
+            isMod = 1;
             break;
         case '2':
             puts("Please enter Student's Name:");
@@ -298,26 +324,31 @@ void Stu_Modify(List *plist)
                 break;
             }
             uppercase(fnode->item.Name);
+            isMod = 1;
             break;
         case '3':
             puts("Please enter Student's C language grade:");
             if (!Get_Int(&fnode->item.grade.C_lang))
                 break;
+            isMod = 1;
             break;
         case '4':
             puts("Please enter Student's Math grade:");
             if (!Get_Int(&fnode->item.grade.Math))
                 break;
+            isMod = 1;
             break;
         case '5':
             puts("Please enter Student's English grade:");
             if (!Get_Int(&fnode->item.grade.Eng))
                 break;
+            isMod = 1;
             break;
         case '6':
             puts("Please enter Student's Rating:");
             if (!Get_Int(&fnode->item.Rating))
                 break;
+            isMod = 1;
             break;
         default:
             puts("ERROR: Switching error");
@@ -327,37 +358,45 @@ void Stu_Modify(List *plist)
         Title_Display();
         Item_Display(fnode->item);
     }
+    if (isMod)
+        return true;
+    return false;
 }
 
 char Modify_Menu(void)
 {
-    puts("Please chooce the data you want to modify: ");
+    puts("\n--------------Modify--------------");
     puts("1) ID                2) Name");
     puts("3) C Language grade  4) Math grade");
     puts("5) English grade     6) Rating");
     puts("0) cancel");
-    char ch;
-    while ((ch = getchar()) != EOF)
+    puts("Please chooce the data you want to modify:");
+    char ch[2];
+    while (s_gets(ch, 2) != NULL && ch[0] != '\0')
     {
-        while (getchar() != '\n')
-            continue;
-        ch = tolower(ch);
-        if (strchr("0123456", ch) == NULL)
+        // while (getchar() != '\n')
+        //     continue;
+        ch[0] = tolower(ch[0]);
+        if (strchr("0123456", ch[0]) == NULL)
             puts("ERROR: Please enter 1 ~ 6 or 0:");
         else
             break;
     }
-    if (ch == EOF)
-        ch = '0';
-    return ch;
+    if (ch[0] == '\0')
+    {
+        ch[0] = '0';
+        puts("Operation canceled.");
+    }
+    return ch[0];
 }
 
-void Stu_Sort(List *plist)
+bool Stu_Sort(List *plist)
 {
+    bool isSort = 0;
     if (ListIsEmpty(plist))
     {
         puts("ERROR: No entries!");
-        return;
+        return false;
     }
     Stu_Display(plist);
     char chooce;
@@ -367,91 +406,109 @@ void Stu_Sort(List *plist)
         {
         case '1':
             ListSort(plist, Cmp_bID);
+            isSort = 1;
             break;
         case '2':
             ListSort(plist, Cmp_bName);
+            isSort = 1;
             break;
         case '3':
             ListSort(plist, Cmp_bCG);
+            isSort = 1;
             break;
         case '4':
             ListSort(plist, Cmp_bMG);
+            isSort = 1;
             break;
         case '5':
             ListSort(plist, Cmp_bEG);
+            isSort = 1;
             break;
         case '6':
             ListSort(plist, Cmp_bTG);
+            isSort = 1;
             break;
         case '7':
             ListSort(plist, Cmp_bRat);
+            isSort = 1;
             break;
         }
         Stu_Display(plist);
     }
+    if (isSort)
+        return true;
+    return false;
 }
 
 char Sort_Menu(void)
 {
-    puts("Please chooce the Sorting criteria: ");
+    puts("\n--------------------Sort--------------------");
     puts("1) ID                2) Name");
     puts("3) C Language grade  4) Math grade");
     puts("5) English grade     6) Average/Total grade");
     puts("7) Rating            0) cancel");
-    char ch;
-    while ((ch = getchar()) != EOF)
+    puts("Please chooce the Sorting criteria(1 ~ 9 or 0):");
+    char ch[2];
+    while (s_gets(ch, 2) != NULL && ch[0] != '\0')
     {
-        while (getchar() != '\n')
-            continue;
-        ch = tolower(ch);
-        if (strchr("01234567", ch) == NULL)
-            puts("ERROR: Please enter 1 ~ 7 or 0:");
+        ch[0] = tolower(ch[0]);
+        if (strchr("01234567", ch[0]) == NULL)
+            puts("ERROR: Please enter 1 ~ 7 or 0: ");
         else
             break;
     }
-    if (ch == EOF)
-        ch = '0';
-    return ch;
+    if (ch[0] == '\0')
+    {
+        ch[0] = '0';
+        puts("Operation canceled.");
+    }
+    return ch[0];
 }
 
-void Stu_insert(List *plist)
+bool Stu_insert(List *plist)
 {
-    Stu_Display(plist);
+    if (!Stu_Display(plist))
+        return false;
     if (ListIsFull(plist))
     {
         puts("ERROR: No space in the program!");
-        return;
+        return false;
     }
 
     Item temp;
     Node *fnode;
     puts("Select the location to insert:");
     if (!Get_ID(temp.StuID))
-        return;
+        return false;
     fnode = ListSeekSet(&temp, plist, seek_bID);
     if (fnode == NULL)
     {
         printf("ERROR: ID %s is not a member.\n", temp.StuID);
-        return;
+        return false;
     }
     printf("Data will be insert before ID %s.\n", temp.StuID);
     if (!Input_Item(&temp, plist))
-        return;
+        return false;
     if (ListInsertItem(&temp, fnode, plist))
+    {
         puts("Insert successful.");
+        return true;
+    }
     else
         puts("ERROR: Fail to insert record.");
+    return false;
 }
 
-void Stu_Display(const List *plist)
+bool Stu_Display(const List *plist)
 {
     if (ListIsEmpty(plist))
-        puts("ERROR: No entries!");
-    else
     {
-        Title_Display();
-        ListTraverse(plist, Item_Display, 0);
+        puts("ERROR: No entries!");
+        return false;
     }
+    Title_Display();
+    ListTraverse(plist, Item_Display, 0);
+    return true;
 }
 
 void Title_Display(void)
@@ -538,4 +595,18 @@ void Stu_Saved(const List *plist)
         if (fclose(fp) != 0)
             fprintf(stderr, "Erroe closing file.\n");
     }
+}
+
+void Stu_Exit(List *plist)
+{
+    if (unsaved == 1)
+    {
+        puts("File is not save.");
+        printf("Do you want to save it?(y/n): ");
+        char temp[2];
+        if (s_gets(temp, 2) == NULL || temp[0] == '\0' || strchr("yY", temp[0]) == NULL)
+            return;
+        Stu_Saved(plist);
+    }
+    return;
 }
