6c438102e6c12b5b65e1d8d2d2061a34bf305f45
ctuu <ct.liuu@gmail.com>
2017-09-11 10:27:22 +0800

    commit

--- temp.c
+++ temp.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-    printf("adfs\rbdfjo");
-    int i;
-    scanf("%d", &i);
+   char  *language[]={"FORTRAN","BASIC","PASCAL", "JAVA","C"};
+   char lang[5][10]={"FORTRAN","BASIC","PASCAL", "JAVA","C"};
+   printf(" : %s\n", *language);
+   printf(" : %s\n", lang);
+   printf(" : %s\n", *lang);
+   printf(" : %p\n", language);
+   printf(" : %c\n", *language[2]);
+   printf(" : %p\n", language + 2);
+   printf(" : %p    %p\n", *language, language[0]);
+   printf(" : %p    %p    %p\n", lang, *lang, lang[0]);
     return 0;
 }
